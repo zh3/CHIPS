d5f830d4b87b449f748cb3a1a44254a45f23c866
Tom He <tomhe123@hotmail.com>
2012-02-05 22:52:59 -0800

    Added rudimentary barcode scanner functionality

---
+++ CHIPSApp/src/com/Chips/ApplicationHubActivity.java
@@ -0,0 +1,41 @@
+package com.Chips;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ApplicationHubActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.application_hub);
+    }
+    
+    // Callback for barcode scanner activity
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        Toast toast;
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            toast = Toast.makeText(getApplicationContext(), scanResult.toString(), Toast.LENGTH_LONG);
+            
+        } else {
+            toast = Toast.makeText(getApplicationContext(), "no result", Toast.LENGTH_LONG);
+        }
+        // else continue with any other code you need in the method
+        toast.show();
+    }
+    
+    /*
+     * Callback function for when 'Scan Barcode' is clicked
+     */
+    public void scanBarcodeClicked(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+}
