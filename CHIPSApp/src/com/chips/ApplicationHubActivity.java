36439213e70eca8c7460fd4a6c257ad9a0e008ad
Tom He <tomhe123@hotmail.com>
2012-02-21 12:33:06 -0800

    Integrated barcode scanner functionality to new layout

--- CHIPSApp/src/com/chips/ApplicationHubActivity.java
+++ CHIPSApp/src/com/chips/ApplicationHubActivity.java
@@ -4,11 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
-import com.chips.R;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 public class ApplicationHubActivity extends Activity {
     /** Called when the activity is first created. */
@@ -27,28 +22,6 @@ public class ApplicationHubActivity extends Activity {
         inventoryActivityIntent = new Intent(this, InventoryActivity.class);
         preferencesActivityIntent = new Intent(this, PreferencesActivity.class);
         statisticsActivityIntent = new Intent(this, StatisticsActivity.class);
-    }
-    
-    // Callback for barcode scanner activity
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        Toast toast;
-        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (scanResult != null) {
-            toast = Toast.makeText(getApplicationContext(), scanResult.toString(), Toast.LENGTH_LONG);
-            
-        } else {
-            toast = Toast.makeText(getApplicationContext(), "no result", Toast.LENGTH_LONG);
-        }
-        // else continue with any other code you need in the method
-        toast.show();
-    }
-    
-    /*
-     * Callback function for when 'Scan Barcode' is clicked
-     */
-    public void scanBarcodeClicked(View view) {
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.initiateScan();
     }
     
     public void searchFoodClicked(View view) {
--- CHIPSApp/src/com/chips/InventoryActivity.java
+++ CHIPSApp/src/com/chips/InventoryActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class InventoryActivity extends Activity {
     /** Called when the activity is first created. */
@@ -49,5 +53,27 @@ public class InventoryActivity extends Activity {
             = new Intent(this, ApplicationHubActivity.class);
         applicationHubActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(applicationHubActivityIntent);
+    }
+    
+    /*
+     * Callback function for when 'Scan Barcode' is clicked
+     */
+    public void scanBarcodeClicked(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+    
+    // Callback for barcode scanner activity
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        Toast toast;
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            toast = Toast.makeText(getApplicationContext(), scanResult.toString(), Toast.LENGTH_LONG);
+            
+        } else {
+            toast = Toast.makeText(getApplicationContext(), "no result", Toast.LENGTH_LONG);
+        }
+        // else continue with any other code you need in the method
+        toast.show();
     }
 }
