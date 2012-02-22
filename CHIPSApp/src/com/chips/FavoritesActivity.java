2047bc49779767d9473f939bda7615b1110cee6e
Jacob Norris <jacobnorris44@gmail.com>
2012-02-21 19:28:50 -0800

    skeleton "favorites" activity

---
+++ CHIPSApp/src/com/chips/FavoritesActivity.java
@@ -0,0 +1,43 @@
+package com.chips;
+
+import android.os.Bundle;
+
+public class FavoritesActivity extends HomeBarActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favorites);
+    }
+    
+    // super calls for basic activity-changing functions.
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+}
