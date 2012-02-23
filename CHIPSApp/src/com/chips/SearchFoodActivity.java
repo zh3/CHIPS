c2126e51834f11251b489daed17435bf18943b38
Tom He <tomhe123@hotmail.com>
2012-02-22 20:09:51 -0800

    Abstracted food search activity

--- CHIPSApp/src/com/chips/SearchFoodActivity.java
+++ CHIPSApp/src/com/chips/SearchFoodActivity.java
@@ -1,21 +1,11 @@
 package com.chips;
 
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.chips.dataclient.FoodClient;
-import com.chips.datarecord.FoodRecord;
-
-public class SearchFoodActivity extends Activity implements Observer {
+public class SearchFoodActivity extends AsynchronousFoodRecordListViewActivity {
     private static final String BASE_SEARCH_URL 
         = "http://cs110chips.phpfogapp.com/index.php/mobile/list_foods_in_nutrition_database_with_name/";
     
@@ -26,54 +16,60 @@ public class SearchFoodActivity extends Activity implements Observer {
         setContentView(R.layout.search_food);
         
         searchFoodEditText = (EditText) findViewById(R.id.searchFoodEditText);
-        searchClient = new FoodClient();
+//        searchClient = new FoodClient();
         loadFoundItems();
     }
     
-    @Override
-    protected void onPause() {
-        super.onPause();
-        searchClient.deleteObserver(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        searchClient.addObserver(this);
-        update(null,null);
-        searchClient.refreshClient();
-    }
-    
-    public void update(Observable dataClient, Object data) {
-        foodRecordAdapter.notifyDataSetChanged();
-        
-        List<?> list = searchClient.getFoodRecords();
-        if (list.size() == 0) {
-            Toast.makeText(SearchFoodActivity.this, 
-                    "No items found", 
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-    
-    public void loadFoundItems() {
-        ListView foundItemsView 
-            = (ListView) findViewById(R.id.searchResultView);
-        
-        foodRecordAdapter = new ArrayAdapter<FoodRecord>(this,
-                android.R.layout.simple_list_item_1, 
-                    searchClient.getFoodRecords());
-        
-        foundItemsView.setAdapter(foodRecordAdapter);
-    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        searchClient.deleteObserver(this);
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        searchClient.addObserver(this);
+//        update(null,null);
+//        searchClient.refreshClient();
+//    }
+//    
+//    public void update(Observable dataClient, Object data) {
+//        foodRecordAdapter.notifyDataSetChanged();
+//        
+//        List<?> list = searchClient.getFoodRecords();
+//        if (list.size() == 0) {
+//            Toast.makeText(SearchFoodActivity.this, 
+//                    "No items found", 
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//    
+//    public void loadFoundItems() {
+//        ListView foundItemsView 
+//            = (ListView) findViewById(R.id.searchResultView);
+//        
+//        foodRecordAdapter = new ArrayAdapter<FoodRecord>(this,
+//                android.R.layout.simple_list_item_1, 
+//                    searchClient.getFoodRecords());
+//        
+//        foundItemsView.setAdapter(foodRecordAdapter);
+//    }
     
     public void doSearchFoodButtonClicked(View view) {
-        searchClient.setURL(
+        client.setURL(
                 BASE_SEARCH_URL + searchFoodEditText.getText().toString()
         );
-        searchClient.refreshClient();
+        client.refreshClient();
     }
     
-    private ArrayAdapter<FoodRecord> foodRecordAdapter;
+    @Override
+    protected ListView getListView() {
+        // TODO Auto-generated method stub
+        return (ListView) findViewById(R.id.searchResultView);
+    }
+    
+//    private ArrayAdapter<FoodRecord> foodRecordAdapter;
     private EditText searchFoodEditText;
-    private FoodClient searchClient;
+//    private FoodClient searchClient;
 }
