3f23c3d7022c7d548a5842c73e520961e207d70e
Tom He <tomhe123@hotmail.com>
2012-02-24 16:28:43 -0800

    Removed outdated code

--- CHIPSApp/src/com/chips/ApplicationHubActivity.java
+++ CHIPSApp/src/com/chips/ApplicationHubActivity.java
@@ -4,11 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.Gallery;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.chips.adapters.MealDisplayAdapter;
 
@@ -32,125 +28,31 @@ public class ApplicationHubActivity extends Activity {
         inventoryActivityIntent = new Intent(this, InventoryActivity.class);
         preferencesActivityIntent = new Intent(this, PreferencesActivity.class);
         statisticsActivityIntent = new Intent(this, StatisticsActivity.class);
-        
-        // Initialize buttons --
-//        searchFoodClicked();
-        shoppingListClicked();
-        calendarClicked();
-        inventoryClicked();
-        preferencesClicked();
-        statisticsClicked();
-        
     }
-    
-    
-    /*
-    // Callback for barcode scanner activity
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        Toast toast;
-        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (scanResult != null) {
-            toast = Toast.makeText(getApplicationContext(), scanResult.toString(), Toast.LENGTH_LONG);
-            
-        } else {
-            toast = Toast.makeText(getApplicationContext(), "no result", Toast.LENGTH_LONG);
-        }
-        // else continue with any other code you need in the method
-        toast.show();
-    }
-    */
-    
-    
-    /*
-     * Callback function for when 'Scan Barcode' is clicked
-     */
-    /*
-    public void scanBarcodeClicked(View view) {
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.initiateScan();
-    }
-*/
 
-    public void searchFoodClicked() {
-    	// For the search food button.
-        final ImageButton button = (ImageButton) findViewById(R.id.doSearchFoodButton);
-            button.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-            	
-            	Toast.makeText(ApplicationHubActivity.this, "Search for food!", Toast.LENGTH_SHORT).show();
-            	startActivity(searchFoodActivityIntent);
-            }
-            });
+    public void searchFoodClicked(View v) {
+        startActivity(searchFoodActivityIntent);
     }
     
-    public void shoppingListClicked() {
-    	// For the Shopping List button.
-        final ImageButton button1 = (ImageButton) findViewById(R.id.shoppingButton);
-            button1.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-            	
-            	Toast.makeText(ApplicationHubActivity.this, "Going to shopping lists!", Toast.LENGTH_SHORT).show();
-            	startActivity(shoppingListActivityIntent);
-            }
-            }); 
+    public void shoppingListClicked(View v) {
+        startActivity(shoppingListActivityIntent);
     }
     
-    public void calendarClicked() {
-    	// For the Calendar button.
-        final ImageButton button2 = (ImageButton) findViewById(R.id.calendarButton);
-            button2.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-            	
-            	Toast.makeText(ApplicationHubActivity.this, "Going to calendar!", Toast.LENGTH_SHORT).show();
-            	
-            	//Intent intent = new Intent(ApplicationHubActivity.this, CalendarActivity.class);
-            	startActivity(calendarActivityIntent);
-            }
-            }); 
-            // -----------------------
-        //startActivity(calendarActivityIntent);
+    public void calendarClicked(View v) {
+        startActivity(calendarActivityIntent);
+
     }
     
-    public void inventoryClicked() {
-    	// For the Inventory button.
-        final ImageButton button3 = (ImageButton) findViewById(R.id.inventoryButton);
-            button3.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-            	
-            	Toast.makeText(ApplicationHubActivity.this, "Going to inventory!", Toast.LENGTH_SHORT).show();
-            	startActivity(inventoryActivityIntent);
-            }
-            }); 
+    public void inventoryClicked(View v) {
+        startActivity(inventoryActivityIntent);
     }
     
-    public void preferencesClicked() {
-    	// For the Preferences button.
-        final ImageButton button4 = (ImageButton) findViewById(R.id.applicationHubSettingsButton);
-            button4.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-            	
-            	Toast.makeText(ApplicationHubActivity.this, "Going to preferences!", Toast.LENGTH_SHORT).show();
-            	startActivity(preferencesActivityIntent);
-            }
-            }); 
+    public void preferencesClicked(View v) {
+        startActivity(preferencesActivityIntent);
     }
     
-    public void statisticsClicked() {
-    	// For the Statistics button.
-        final ImageButton button5 = (ImageButton) findViewById(R.id.addButton);
-//            button5.setOnClickListener(new OnClickListener() {
-//            public void onClick(View v) {
-                // Perform action on clicks
-            	
-//            	Toast.makeText(ApplicationHubActivity.this, "Going to statistics!", Toast.LENGTH_SHORT).show();
-//            	startActivity(statisticsActivityIntent);
-//            }
-//            }); 
+    public void statisticsClicked(View v) {
+        startActivity(statisticsActivityIntent);
     }
     
     public void addFavoriteClicked(View view) {
