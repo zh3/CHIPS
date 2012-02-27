312c3723daccf85da858950b52279c446ae9b2f5
Tom He <tomhe123@hotmail.com>
2012-02-26 20:42:33 -0800

    Added push functionality to android add

--- CHIPSApp/src/com/chips/AddFoodToInventoryActivity.java
+++ CHIPSApp/src/com/chips/AddFoodToInventoryActivity.java
@@ -1,5 +1,7 @@
 package com.chips;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,20 +9,27 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.chips.dataclient.DataPushClient;
 import com.chips.datarecord.FoodRecord;
 import com.chips.homebar.HomeBar;
 import com.chips.homebar.HomeBarAction;
+import com.chips.user.PersistentUser;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class AddFoodToInventoryActivity extends Activity implements HomeBar {
     private static final int SEARCH_REQUEST_CODE = 0;
+    private static final String ASSIGN_BARCODE_TO_FOOD_URL
+    = "http://cs110chips.phpfogapp.com/index.php/mobile/" 
+            + "assign_barcode_to_food/";
     
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         HomeBarAction.inflateHomeBarView(this, R.layout.add_food_to_inventory);
+        
+        pushClient = new DataPushClient();
         
         searchFoodIntent = new Intent(this, SearchFoodActivity.class);
         setupEditTexts();
@@ -81,13 +90,22 @@ public class AddFoodToInventoryActivity extends Activity implements HomeBar {
             Intent intent) {
         Toast toast;
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        ArrayList<String> assignBarcodeArguments = new ArrayList<String>();
+        
         if (scanResult != null) {
             toast = Toast.makeText(getApplicationContext(), scanResult.toString(), Toast.LENGTH_LONG);
             
+            assignBarcodeArguments.add(PersistentUser.getSessionID());
+            assignBarcodeArguments.add(scanResult.getContents());
+            assignBarcodeArguments.add(scanResult.getFormatName());
+            assignBarcodeArguments.add("1001");
+            
+            pushClient.setURL(ASSIGN_BARCODE_TO_FOOD_URL, assignBarcodeArguments);
+            pushClient.refreshClient();
         } else {
             toast = Toast.makeText(getApplicationContext(), "no result", Toast.LENGTH_LONG);
         }
-        // else continue with any other code you need in the method
+        
         toast.show();
     }
     
@@ -114,4 +132,5 @@ public class AddFoodToInventoryActivity extends Activity implements HomeBar {
     private EditText proteinField;
     private EditText fatField;
     private EditText quantityField;
+    private DataPushClient pushClient;
 }
