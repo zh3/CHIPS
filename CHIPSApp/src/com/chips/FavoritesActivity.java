f4016d25c2f39da34d46da291c55c56e0fbab401
Jacob Norris <jacobnorris44@gmail.com>
2012-03-07 17:27:19 -0800

    renamed FavoritesActivity, created new Favorites hub

--- CHIPSApp/src/com/chips/FavoritesActivity.java
+++ CHIPSApp/src/com/chips/FavoritesActivity.java
@@ -1,97 +1,331 @@
 package com.chips;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ExpandableListView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-
-import com.chips.adapters.ExpandableFoodListAdapter;
+import com.chips.dataclient.DataPushClient;
 import com.chips.dataclient.FoodClient;
-import com.chips.dataclientobservers.ExpandableFoodClientObserver;
+import com.chips.dataclientactions.PushClientToastOnFailureAction;
+import com.chips.dataclientobservers.UpdateActionDataClientObserver;
 import com.chips.datarecord.FoodRecord;
 import com.chips.homebar.HomeBar;
 import com.chips.homebar.HomeBarAction;
 import com.chips.user.PersistentUser;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class FavoritesActivity extends DataClientActivity implements HomeBar, Serializable {
-
-	//import com.chips.adapters.ExpandableFavoritesAdapter; up top ^^
-	
-	private static final String BASE_URL 
+public class FavoritesActivity extends DataClientActivity
+        implements HomeBar {
+    private static final int SEARCH_REQUEST_CODE = 0;
+    private static final String BASE_URL 
       = "http://cs110chips.phpfogapp.com/index.php/mobile/";
     private static final String FAVORITES_LIST_URL 
       = BASE_URL + "list_favorite_meals";
     private static final String ADD_FOOD_TO_FAVORITES_URL
       = BASE_URL + "add_meal_to_favorites/";
-
-	private static final int SEARCH_REQUEST_CODE = 0;
-	
-	private List<FoodRecord> currentMeal = new ArrayList<FoodRecord>();
-	
+    public static final String BUNDLE_ADD_KEY = "addURL";
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         HomeBarAction.inflateHomeBarView(this, R.layout.favorites);
         
-        FoodClient foodClient = new FoodClient();
-        ExpandableFoodClientObserver expandableFoodClientObserver
-            = new ExpandableFoodClientObserver(this, foodClient);
+        setupAddURL();
+        setupWebsiteCommunication();
         
-        addClientObserverPair(foodClient, expandableFoodClientObserver);
-        
-        ExpandableListView favoritesView 
-            = (ExpandableListView) findViewById(R.id.favoriteMealListView);
-        expandableFoodClientObserver.setListViewLayout(
-            favoritesView, 
-            new ExpandableFoodListAdapter(this, currentMeal, 
-                    favoritesView)
-        );
-        
-//        foodClient.setURL(FAVORITES_LIST_URL, PersistentUser.getSessionID());
-
-//        foodClient.asynchronousLoadClientData();
-        setupIntents();
-        
-        // onActivityResult call?
+        addMealIntent = new Intent(this, AddMealToFavoritesActivity.class);
+//        handleLinkBarcode = false;
+//        setupEditTexts();
     }
     
-    private void setupIntents() {
-    	Bundle b = new Bundle();
-        b.putString(AddFoodActivity.BUNDLE_ADD_KEY, 
-                ADD_FOOD_TO_FAVORITES_URL);
-
-        addFoodToFavoriteIntent 
-            = new Intent(this, AddFoodToFavoriteMealActivity.class);
-        addFoodToFavoriteIntent.putExtras(b);
+    private void setupAddURL() {
+        Bundle b = getIntent().getExtras();
+        
+        addURL = b.getString(BUNDLE_ADD_KEY);
     }
     
+    private void setupWebsiteCommunication() {
+        pushClient = new DataPushClient();
+        foodClient = new FoodClient();
+        UpdateActionDataClientObserver updateActionObserver 
+            = new UpdateActionDataClientObserver(this, pushClient);
+        Toast failureToast = Toast.makeText(this, 
+                "Website Communication Failed", Toast.LENGTH_LONG);
+        PushClientToastOnFailureAction action 
+            = new PushClientToastOnFailureAction(failureToast);
+        updateActionObserver.addAction(action);
+        
+        addClientObserverPair(pushClient, updateActionObserver);
+    }
+    
+/*    
+    private void setupEditTexts() {
+        nameField = (EditText) findViewById(R.id.newFoodNameEditText);
+        caloriesField = (EditText) findViewById(R.id.caloriesEditText);
+        carbohydratesField 
+            = (EditText) findViewById(R.id.carbohydratesEditText);
+        proteinField = (EditText) findViewById(R.id.proteinEditText);
+        fatField = (EditText) findViewById(R.id.fatEditText);
+        quantityField = (EditText) findViewById(R.id.quantityEditText);
+        barcodeFormatField 
+            = (EditText) findViewById(R.id.barcodeFormatEditText);
+        barcodeField 
+            = (EditText) findViewById(R.id.barcodeEditText);
+    }
+*/  
+    
+    
+    /*
+     * Callback function for when 'Scan Barcode' is clicked
+     */
+/*    
+    public void scanBarcodeClicked(View view) {
+        startBarcodeScanner();
+        handleLinkBarcode = false;
+    }
+    
+    public void linkBarcodeClicked(View view) {
+        startBarcodeScanner();
+        handleLinkBarcode = true;
+    }
+    
+    private void startBarcodeScanner() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+    
+    // Callback for barcode scanner activity
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-      if (requestCode == 0) {
+        switch (requestCode) {
+        case SEARCH_REQUEST_CODE:
+            handleSearchResult(requestCode, resultCode, intent);
+            break;
+        default:
+            handleScanResult(requestCode, resultCode, intent);
+        }
+    }
+  
+*/   // Shouldn't need any barcode scanning here.
+    
+/*
+    
+    private void handleSearchResult(int requestCode, int resultCode, 
+            Intent intent) {
         if (resultCode == RESULT_OK) {
-        
-        Bundle extras = intent.getExtras();
-        FoodRecord selectedFood = (FoodRecord) extras.get("selectedFood");
-        
-        currentMeal.add(selectedFood);
-        
-        } else if (resultCode == RESULT_CANCELED) {
-        	// Handle cancel
-        	}
-      }
+            Bundle extras = intent.getExtras();
+            FoodRecord selectedFood = (FoodRecord) extras.get("selectedFood");
+            
+            foodToAdd = selectedFood;
+            populateFields(selectedFood);
+        }
+    }
+
+*/    
+    
+    private void populateFields(FoodRecord food) {
+        if (food != null) {
+            nameField.setText(food.getName());
+            caloriesField.setText(Double.toString(food.getCalories()));
+            carbohydratesField.setText(Double.toString(food.getCarbohydrates()));
+            proteinField.setText(Double.toString(food.getProtein()));
+            fatField.setText(Double.toString(food.getFat()));
+            
+            setNutritionFieldsEnabled(false);
+        }
     }
     
-    public void addFoodToFavoriteClicked(View view) {
-        startActivityForResult(addFoodToFavoriteIntent, SEARCH_REQUEST_CODE);
+    private void setNutritionFieldsEnabled(boolean enabled) {
+        nameField.setEnabled(enabled);
+        nameField.setFocusable(enabled);
+        caloriesField.setEnabled(enabled);
+        caloriesField.setFocusable(enabled);
+        carbohydratesField.setEnabled(enabled);
+        carbohydratesField.setFocusable(enabled);
+        proteinField.setEnabled(enabled);
+        proteinField.setFocusable(enabled);
+        fatField.setEnabled(enabled);
+        fatField.setFocusable(enabled);
     }
     
-    public void saveFavoriteClicked(View view) {
-    	// TODO push the new favorite meal to the website here.
+/*    
+    
+    private void handleScanResult(int requestCode, int resultCode, 
+            Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+//        ArrayList<String> assignBarcodeArguments = new ArrayList<String>();
+        
+        if (scanResult == null || scanResult.getContents() == null) return;
+        
+        if (!handleLinkBarcode)  {
+            // handle barcode lookup
+            List<FoodRecord> foods = synchronousLookupFoodsWithBarcode(scanResult.getContents(), 
+                    scanResult.getFormatName());
+            if (foods.size() > 0) {
+                getUserFoodChoice(foods);
+            } else {
+                Toast.makeText(this, "No foods with that barcode found." 
+                        + "Please enter nutritional information manually", 
+                        Toast.LENGTH_LONG);
+            }
+        }
+
+        barcodeField.setText(scanResult.getContents());
+        barcodeFormatField.setText(scanResult.getFormatName());
+    }
+
+*/
+    
+/*    
+    
+    private List<FoodRecord> synchronousLookupFoodsWithBarcode(String barcode,
+            String format) {
+        
+        ArrayList<String> assignBarcodeArguments = new ArrayList<String>();
+        assignBarcodeArguments.add(PersistentUser.getSessionID());
+        assignBarcodeArguments.add(barcode);
+        assignBarcodeArguments.add(format);
+        
+        foodClient.setURL(GET_FOOD_WITH_BARCODE_URL, 
+                  assignBarcodeArguments);
+        foodClient.logURL();
+        foodClient.synchronousLoadClientData();
+        
+        return foodClient.getFoodRecords();
+    }
+    
+    private void getUserFoodChoice(List<FoodRecord> foods) {
+        String[] foodNames = new String[foods.size()];
+        
+        for (int i = 0; i < foods.size(); i++) {
+            foodNames[i] = foods.get(i).toString();
+        }
+
+        if (foods.size() > 1) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Multiple foods with given barcode found:");
+            builder.setItems(foodNames, 
+                    new SelectFoodWithBarcodeListener(foods));
+            AlertDialog alert = builder.create();
+            alert.show();
+        } else {
+            foodToAdd = foods.get(0);
+            populateFields(foodToAdd);
+        }
+    }
+
+*/    
+    
+    public void addFoodToInventoryClicked(View view) {
+        ImageButton addFoodToInventoryButton 
+            = (ImageButton) findViewById(R.id.addButton);
+        addFoodToInventoryButton.requestFocus();
+        
+        if (missingFoodFieldValuesExist()) {
+            Toast.makeText(this, 
+                    "Please fill name and all nutritional information for "
+                    + "new food", 
+                    Toast.LENGTH_LONG).show();
+        }
+        
+        if (foodToAdd == null) {
+            boolean success = createFoodFromFields();
+            if (!success) return;
+        }
+        
+        if (pushFoodToAddToInventory()) finish();
+    }
+    
+    private boolean createFoodFromFields() {
+        ArrayList<String> createFoodArguments = new ArrayList<String>();
+        createFoodArguments.add(PersistentUser.getSessionID());
+        createFoodArguments.add(nameField.getText().toString());
+        createFoodArguments.add(caloriesField.getText().toString());
+        createFoodArguments.add(carbohydratesField.getText().toString());
+        createFoodArguments.add(fatField.getText().toString());
+        createFoodArguments.add(proteinField.getText().toString());
+        
+        foodClient.setURL(FAVORITES_LIST_URL, createFoodArguments);
+        foodClient.logURL();
+        foodClient.synchronousLoadClientData();
+
+        // make foodToAdd the returned food
+        List<FoodRecord> createdFoodList = foodClient.getFoodRecords();
+        boolean success = (createdFoodList.size() > 0);
+        
+        if (success) {
+            foodToAdd = createdFoodList.get(0);
+        } else {
+            Toast.makeText(this, "Communication Error", 
+                    Toast.LENGTH_LONG).show();
+        }
+        
+        return success;
+    }
+    
+    private boolean pushFoodToAddToInventory() {
+        ArrayList<String> addFoodArguments = new ArrayList<String>();
+        addFoodArguments.add(PersistentUser.getSessionID());
+        addFoodArguments.add(foodToAdd.getId() + "");
+        addFoodArguments.add(quantityField.getText().toString());
+
+/*        
+        String barcode = barcodeField.getText().toString();
+        String barcodeFormat = barcodeField.getText().toString();
+        if (!barcode.equals("") && !barcodeFormat.equals("")) {
+            assignBarcodeToFood(barcodeField.getText().toString(), 
+                    barcodeFormatField.getText().toString(), 
+                    foodToAdd.getId() + "");
+        }
+*/        
+        
+        pushClient.setURL(addURL, addFoodArguments);
+        pushClient.logURL();
+        pushClient.synchronousLoadClientData();
+        
+        boolean success = pushClient.lastCompletedPushSuccessful();
+        if (!success) {
+            Toast.makeText(this, "Communication Error", 
+                    Toast.LENGTH_LONG).show();
+        }
+        
+        return success;
+    }
+
+/*    
+    
+    private void assignBarcodeToFood(String barcode, String barcodeFormat, 
+            String foodID) {
+        ArrayList<String> assignBarcodeArguments = new ArrayList<String>();
+        assignBarcodeArguments.add(PersistentUser.getSessionID());
+        assignBarcodeArguments.add(barcode);
+        assignBarcodeArguments.add(barcodeFormat);
+        assignBarcodeArguments.add(foodID);
+          
+        pushClient.setURL(ASSIGN_BARCODE_TO_FOOD_URL, 
+                  assignBarcodeArguments);
+        pushClient.logURL();
+        pushClient.synchronousLoadClientData();
+    }
+
+*/    
+
+    private boolean missingFoodFieldValuesExist() {
+        return (nameField.getText().toString().equals("")
+                    || caloriesField.getText().toString().equals("")
+                    || carbohydratesField.getText().toString().equals("")
+                    || proteinField.getText().toString().equals("")
+                    || fatField.getText().toString().equals("")
+                    || quantityField.getText().toString().equals(""));
     }
     
     public void goHomeClicked(View view) {
@@ -102,5 +336,42 @@ public class FavoritesActivity extends DataClientActivity implements HomeBar, Se
         HomeBarAction.addFavoriteClicked(this, view);
     }
     
-    private Intent addFoodToFavoriteIntent;
+    public void addFavoriteMealClicked(View view) {
+        startActivityForResult(addMealIntent, SEARCH_REQUEST_CODE);
+    }
+    
+/*  
+    
+    private class SelectFoodWithBarcodeListener 
+            implements DialogInterface.OnClickListener {
+        public SelectFoodWithBarcodeListener(List<FoodRecord> selectableFoods) {
+            this.selectableFoods = selectableFoods;
+        }
+        
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            foodToAdd = selectableFoods.get(which);
+            populateFields(foodToAdd);
+        }
+        
+        private List<FoodRecord> selectableFoods;
+        
+    }
+    
+*/    
+    
+    private Intent addMealIntent;
+    private EditText nameField;
+    private EditText caloriesField;
+    private EditText carbohydratesField;
+    private EditText proteinField;
+    private EditText fatField;
+    private EditText quantityField;
+    private EditText barcodeField;
+    private EditText barcodeFormatField;
+    private DataPushClient pushClient;
+    private FoodClient foodClient;
+    private FoodRecord foodToAdd;
+    private String addURL;
+//    private boolean handleLinkBarcode;
 }
