9e2bd4e3cde1464fd827db37d9fd7a9e46a2787f
Tom He <tomhe123@hotmail.com>
2012-03-08 16:25:27 -0800

    fixed redundant import

--- CHIPSApp/src/com/chips/homebar/HomeBarAction.java
+++ CHIPSApp/src/com/chips/homebar/HomeBarAction.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.chips.ApplicationHubActivity;
-import com.chips.AddMealToFavoritesActivity;
 import com.chips.FavoritesActivity;
 import com.chips.R;
 
